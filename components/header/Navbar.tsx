0581222519c509ddeeaf41086961c224349c5548
Rodrigo Lima <rodrigo@capyba.com>
2023-05-21 12:00:03 -0300

    Add logo to navbar

--- components/header/Navbar.tsx
+++ components/header/Navbar.tsx
@@ -1,5 +1,6 @@
 import Searchbar from "$store/islands/HeaderSearchbar.tsx";
 import Buttons from "$store/islands/HeaderButton.tsx";
+import Logo from "$store/components/Logo.tsx";
 import Icon from "$store/components/ui/Icon.tsx";
 import NavItem from "./NavItem.tsx";
 import { navbarHeight } from "./constants.ts";
@@ -24,7 +25,7 @@ function Navbar({ items, searchbar, enableSearch }: {
           style={{ minHeight: navbarHeight }}
           aria-label="Store logo"
         >
-          <Icon id="Logo" width={126} height={16} />
+          <Logo width={"126"} color={"white"} />
         </a>
 
         <div class="flex gap-1">
@@ -38,7 +39,7 @@ function Navbar({ items, searchbar, enableSearch }: {
       <div class="hidden md:flex flex-row justify-between items-center border-b border-base-200 w-full pl-2 pr-6">
         <div class="flex-none w-44">
           <a href="/" aria-label="Store logo" class="block px-4 py-3 w-[160px]">
-            <Icon id="Logo" width={126} height={16} />
+            <Logo width={"126"} color={"white"} />
           </a>
         </div>
         <div class="flex-auto flex justify-center">
